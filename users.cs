08f1ebdf7d1b4f0f3be5cbd893c54622ab696886
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-07 18:48:48 +0530

    Create users.cs

---
+++ users.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Anvesh = 30; // Integer Variable
+            /* Data Types in C#
+             * Integer  - int Anvesh
+             * Floapting point number - dloat that;
+             * Character - char a = 'A';
+             * Boolean - bool is Great = true;
+             * String - string inp = "Anvesh";
+             */
+            string inp = Console.ReadLine();
+            Console.WriteLine(inp);
+            //Console.WriteLine("Hello World");
+            //Console.Write("Hello Anvesh");
+            //Console.Write("I Love C# and the Number which is my Favourite is:" + Anvesh);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
