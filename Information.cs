08d2117193d11d3bd24122bf4b7fb9e85b987ab7
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-07 18:14:52 +0530

    Create Information.cs

---
+++ Information.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Anvesh");
+            Console.Write("I Love C#");
+            Console.ReadLine();
+        }
+        
+    }
+}
